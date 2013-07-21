896ad872aceb8c67e1bf7cac7e19ccf69e977c46
shaja <aerocomputer@gmail.com>
2013-07-21 17:09:55 +0000

    adding support for reading from file now with fs included 2

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ app.use(express.logger());
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
   fs.readFile('./index.html', function (err, data) {
-  data.toString('utf-8');  
+  //data.toString('utf-8');  
+  data.toString();
 });
 });
 
