d89571427a96cee81b40b44138d116425e8563e8
shaja <aerocomputer@gmail.com>
2013-07-21 16:56:55 +0000

    adding support for reading from file now

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  fs.readFile('./index.html', function (err, data) {
+  data.toString('utf-8')  
+});
 });
 
 var port = process.env.PORT || 5000;
